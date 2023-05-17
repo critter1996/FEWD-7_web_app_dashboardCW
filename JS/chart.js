feb7ffbf4c900eadb1896d76443930d7cd221ac9
critter1996 <109566820+critter1996@users.noreply.github.com>
2023-05-17 17:33:34 -0400

    toggle switch

---
+++ JS/chart.js
@@ -0,0 +1,5 @@
+{/* <script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/4.3.0/chart.umd.js" integrity="sha512-CMF3tQtjOoOJoOKlsS7/2loJlkyctwzSoDK/S40iAB+MqWSaf50uObGQSk5Ny/gfRhRCjNLvoxuCvdnERU4WGg==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
+
+https://cdnjs.cloudflare.com/ajax/libs/Chart.js/4.3.0/chart.umd.js
+
+sha512-CMF3tQtjOoOJoOKlsS7/2loJlkyctwzSoDK/S40iAB+MqWSaf50uObGQSk5Ny/gfRhRCjNLvoxuCvdnERU4WGg== */}
