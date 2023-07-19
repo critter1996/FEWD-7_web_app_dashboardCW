3eb8c5a9eb22a06ee6839eed8f1f6b2f883f92cc
critter1996 <109566820+critter1996@users.noreply.github.com>
2023-07-19 11:35:13 -0400

    Add alert and bell hover

--- JS/app.js
+++ JS/app.js
@@ -2,14 +2,26 @@
 document.addEventListener('DOMContentLoaded', () => {
 
 //variables
+const alertBanner = document.getElementById('alert');
 
 
 
 
+//create the html for the banner 
+alertBanner.innerHTML = 
+`
+<div class="alert-banner">
+    <p><strong>Alert:</strong> You have <strong>6</strong> overdue tasks to complete</p>
+    <p class="alert-banner-close">x</p>
+</div>
+`
 
-
-
-
+alertBanner.addEventListener("click", e => {
+    const element = e.target;
+    if (element.classList.contains("alert-banner-close")) {
+        alertBanner.style.display = "none"
+    }
+});
 
 
 
