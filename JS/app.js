03ff9566a46be9ebdd28a59e4b677b51ae066d67
critter1996 <109566820+critter1996@users.noreply.github.com>
2023-08-08 16:40:13 -0400

    update dropdown

--- JS/app.js
+++ JS/app.js
@@ -202,42 +202,42 @@ send.addEventListener('click', () => {
 
 /* When the user clicks on the button,
 toggle between hiding and showing the dropdown content */
-function myFunction() {
-    document.getElementById("myDropdown").classList.toggle("show");
-  }
+// function myFunction() {
+//     document.getElementById("myDropdown").classList.toggle("show");
+//   }
   
   //myFunction();
 
-  bell.addEventListener("click", e => {
+//   bell.addEventListener("click", e => {
 
-  // Close the dropdown menu if the user clicks outside of it
-    if (!e.target.matches('.dropbtn')) {
-      let i;
-      for (i = 0; i < dropdowns.length; i++) {
-        let openDropdown = dropdowns[i];
-        if (openDropdown.classList.contains('show')) {
-          openDropdown.classList.remove('show');
-        } 
-      }
-    }
-  });
-
-
-// bell.addEventListener("click", e => {
-
+//   // Close the dropdown menu if the user clicks outside of it
 //     if (!e.target.matches('.dropbtn')) {
-//         let i;
-//         for (i = 0; i < dropdowns.length; i++) {
-//           let openDropdown = dropdowns[i];
-//           let isClicked = e.target;
-//             if (isClicked){
-//                 openDropdown.classList.add('show')
-//             } else {
-//                 openDropdown.classList.remove('show')
-//             }
-//         }
+//       let i;
+//       for (i = 0; i < dropdowns.length; i++) {
+//         let openDropdown = dropdowns[i];
+//         if (openDropdown.classList.contains('show')) {
+//           openDropdown.classList.remove('show');
+//         } 
+//       }
 //     }
-// });
+//   });
+
+
+bell.addEventListener("click", e => {
+
+    if (!e.target.matches('.dropbtn')) {
+        let i;
+        for (i = 0; i < dropdowns.length; i++) {
+          let openDropdown = dropdowns[i];
+          let isClicked = e.target;
+            if (isClicked){
+                openDropdown.classList.add('show')
+            } else {
+                openDropdown.classList.remove('show')
+            }
+        }
+    }
+});
 
 // dropdowns.addEventListener('click', e => {
 //     if (!dropdowns.contains(e.target)) {
