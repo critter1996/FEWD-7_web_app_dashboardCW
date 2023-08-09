c43aaca6f2a2ce5510ac2a6c470ae17fc93549a5
critter1996 <109566820+critter1996@users.noreply.github.com>
2023-08-09 10:38:32 -0400

    bell button complete

--- JS/app.js
+++ JS/app.js
@@ -200,29 +200,6 @@ send.addEventListener('click', () => {
 /////////////////////////////////////////////////////////////
 
 
-/* When the user clicks on the button,
-toggle between hiding and showing the dropdown content */
-// function myFunction() {
-//     document.getElementById("myDropdown").classList.toggle("show");
-//   }
-  
-  //myFunction();
-
-//   bell.addEventListener("click", e => {
-
-//   // Close the dropdown menu if the user clicks outside of it
-//     if (!e.target.matches('.dropbtn')) {
-//       let i;
-//       for (i = 0; i < dropdowns.length; i++) {
-//         let openDropdown = dropdowns[i];
-//         if (openDropdown.classList.contains('show')) {
-//           openDropdown.classList.remove('show');
-//         } 
-//       }
-//     }
-//   });
-
-
 bell.addEventListener("click", e => {
 
     if (!e.target.matches('.dropbtn')) {
@@ -231,20 +208,13 @@ bell.addEventListener("click", e => {
           let openDropdown = dropdowns[i];
           let isClicked = e.target;
             if (isClicked){
-                openDropdown.classList.add('show')
+                openDropdown.classList.toggle('show')
             } else {
                 openDropdown.classList.remove('show')
             }
         }
     }
 });
-
-// dropdowns.addEventListener('click', e => {
-//     if (!dropdowns.contains(e.target)) {
-//         dropdowns.style.display = 'none';
-//         openDropdown.style.display = 'none';
-//     }
-// });
 
 
 
